5ca893458a2cee06e91509e3a2fd48f18ee2c237
Emerson <diegowebby@gmail.com>
2021-02-27 11:16:13 -0300

    Add java/JavaRegex.java

---
+++ java/JavaRegex.java
@@ -0,0 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class JavaRegex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+
+    public String pattern = "^(([0-9]|[0-9][0-9]|[01][0-9][0-9]|2[0-4][0-9]|25[0-5]).){3}" + 
+    "([0-9]|[0-9][0-9]|[01][0-9][0-9]|2[0-4][0-9]|25[0-5])$";
+}
