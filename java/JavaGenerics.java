cf5012f4d41132e0e1bca2af77e6401032a1a18d
Emerson <diegowebby@gmail.com>
2021-04-12 13:45:04 -0300

    add JavaGenerics

---
+++ java/JavaGenerics.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   //Write your code here
+   public static <T> void printArray(T[] inputArray){
+
+    for (T element : inputArray){
+        System.out.println(element);
+    }
+
+   }
+ 
+}
+
+ public class JavaGenerics {
+
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
